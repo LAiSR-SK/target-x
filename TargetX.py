33f84e0559475c524d7f8e2848e0a5115b1335d7
The Network & Software Security Research (NSSR) lab <86532171+nssrlab@users.noreply.github.com>
2022-03-15 12:16:44 -0400

    Update TargetX.py
    updated algorithm, most recent changes

--- TargetX.py
+++ TargetX.py
@@ -1,3 +1,4 @@
+# These are the python libraries that will be used
 import numpy as np
 from torch.autograd import Variable
 import torch as torch
@@ -5,23 +6,26 @@ import copy
 import os
 from torch.autograd.gradcheck import zero_gradients
 
-#Combined Hybrid algorithm of Deepfool and FGSM, plus modification to obtain maximum hyperplanes.
 
-def targetx(image, net, eps=0.05, num_classes=1000, overshoot=0.02, max_iter=50):
+# Target-X, a novel targeted white box attack, that seeks to find the minimum perturbations while maximizing the loss function.
+# We define 2 methods "targetx" and "targetx_args" the latter allows the user to pre-define the specific target label for the image
+# to be misclassified as in the function header, whereas the "targetx" method asks the user during runtime.
+
+def targetx(image, net, eps=0.05, num_classes=10, overshoot=0.02, max_iter=50):
     """
-       :param image: Image of size HxWx3
-       :param net: network (input: images, output: values of activation **BEFORE** softmax).
-       :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
-       :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
-       :param max_iter: maximum number of iterations for deepfool (default = 50)
-       :param eps: epsilon value for combination of FGSM.
-       :return: perturbation from hybrid method, number of iterations that it required, new estimated_label, perturbed image, and F_k value (distance moved into new hyperplane)
+        :param image: Image of size HxWx3
+        :param net: network (input: images, output: values of activation **BEFORE** softmax).
+        :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
+        :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
+        :param max_iter: maximum number of iterations for targetx (default = 50)
+        :param eps: epsilon value for combination of FGSM.
+        :return: perturbation from targetx method, number of iterations that it required, new estimated_label, perturbed image, and F_k value (distance moved into new hyperplane)
     """
-    #Check if cuda is available.
+    # Check if cuda is available.
     is_cuda = torch.cuda.is_available()
     ILSVRClabels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
-    #If cuda is available use GPU for faster processing, if not, use CPU.
+    # If cuda is available use GPU for faster processing, if not, use CPU.
     if is_cuda:
         print("Using GPU")
         image = image.cuda()
@@ -29,56 +33,80 @@ def targetx(image, net, eps=0.05, num_classes=1000, overshoot=0.02, max_iter=50)
     else:
         print("Using CPU")
 
-    #Convert image into tensor readable by PyTorch, flatten image.
+    # Convert image into tensor readable by PyTorch, flatten image. f_image is the flattened image
     f_image = net.forward(Variable(image[None, :, :, :], requires_grad=True)).data.cpu().numpy().flatten()
-    #Create array of labels.
-    I = (np.array(f_image)).flatten().argsort()[::-1]
-    #I = np.sort(I)
-    print(I)
-    inputNum = input('Enter number of label for image to be changed to.')
-    newLabel = ILSVRClabels[np.int(inputNum)].split(',')[0]
-    print('Input ID: ' + inputNum, ' Label: ' + newLabel)
 
-    #Define array as size of specified number of classes, set first class to the original label.
-    I = I[0:num_classes]
-    label = I[0]
-    print('I array:')
+    # Create array of labels I.
+    I = (np.array(f_image)).flatten().argsort()[::-1]
+
+    print('I array: ')
     print(I[0:num_classes])
+
+    # target_label is the label number corresponding to the label the image will be misclassified to
+    target_label = input('Enter number of label for image to be changed to.')
+    new_label = ILSVRClabels[np.int(target_label)].split(',')[0]
+    print('Input ID: ' + target_label, ' Label: ' + new_label)
+
+    # Define array as size of specified number of classes
+    I = I[0:num_classes]
+    # Set first class to the original label.
+    orig_label = I[0]
+
+    # print('I array: ')
+    # print(I[0:num_classes])
+
+    print('Original Label: ')
     print(I[0])
 
-
-    #Copy the image, create variable for perturbed image, as well as w and r_tot, using the shape of the image
+    # copy image
     input_shape = image.cpu().numpy().shape
+    # create pert image variable
     pert_image = copy.deepcopy(image)
+    # create leap vector w
     w = np.zeros(input_shape)
+    # create r_tot
     r_tot = np.zeros(input_shape)
 
-    #initialize loop variable to 0
+    # initialize loop variable to 0
     loop_i = 0
 
-    #Set x to the original image, forward propagate it through the network, get list of classes
+    # create a variable x and set it to the original image
     x = Variable(pert_image[None, :], requires_grad=True)
+    # forward propogate imgae x through the network
     fs = net.forward(x)
+    # list of classes fs_list (possible target labels for input image x)
     fs_list = [fs[0, I[k]] for k in range(num_classes)]
+    # print fs_list
     print('fs list')
     print(fs_list)
-    k_i = label
+    # setting the initial k, k_i to the original lable orig_label
+    k_i = orig_label
 
-    #While label has not changed to custom label and max iterations not reached:
-    while k_i != int(inputNum) and loop_i < max_iter:
+    # While label has not changed to custom label and max iterations not reached:
+    while k_i != int(target_label) and loop_i < max_iter:
 
-        #Backwards propagate label through graph, get resulting gradient and gradient sign.
+        # Backwards propagate label through graph, get resulting gradient and gradient sign.
         pert = 0  # np.inf we change the to be zero instead of infinty
         fs[0, I[0]].backward(retain_graph=True)
         grad_orig = x.grad.data.cpu().numpy().copy()
-        grad_orig_sign = x.grad.sign().cpu().numpy().copy()  # added for fgsm
 
         zero_gradients(x)
-             #Backwards propagate current label through graph, get resulting gradient and gradient sign.
-        k = np.where(I == int(inputNum))
+
+        # Backwards propagate current label through graph, get resulting gradient and gradient sign.
+        # k is how we turn the given target label to an index value
+        k = np.where(I == int(target_label))
+        print("correct K: ")
+        print(k)
         k = k[0][0]
         print('k')
         print(k)
+
+        fs[0, I[k]].backward(retain_graph=True)
+        grad_target = x.grad.data.cpu().numpy().copy()
+        target_sign_grad = x.grad.sign().cpu().numpy().copy()
+
+        zero_gradients(x)
+
         fs[0, I[k]].backward(retain_graph=True)
         print(I[k])
         print('0, I[k]')
@@ -86,42 +114,34 @@ def targetx(image, net, eps=0.05, num_classes=1000, overshoot=0.02, max_iter=50)
         cur_grad = x.grad.data.cpu().numpy().copy()
         cur_sign_grad = x.grad.sign().cpu().numpy().copy()  # added for fgsm
 
-        # set new w_k and new f_k
-        w_k = cur_grad - grad_orig
-        w_k_sign = cur_sign_grad - grad_orig_sign  # added for fgsm
+        # set new w_k, distance between original and current gradient, and new f_k, distance between original and target label
+        w = grad_target - grad_orig
         f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
 
-
-        #Calculate perturbation using deepfool formula
-        pert_k = abs(f_k) / np.linalg.norm(w_k.flatten(), ord=1)
-        print('pert_k')
-        print(pert_k)
+        # Calculate perturbation using targetx formula
+        pert = abs(f_k) / np.linalg.norm(w.flatten(), ord=1)
+        print('pert')
+        print(pert)
 
         print('----------')
 
-        # determine which w_k to use
-        if pert_k > pert:  # we change here the "<" to be ">" to get the max hyperplanes
-            pert = pert_k
-            w = w_k
-
         # compute r_i and r_tot
-        # Added 1e-4 for numerical stability
-        r_i = (pert + 1e-4) * w / np.linalg.norm(w)
-        #combined calculated perturbation with FGSM, multiply the gradient by the epsilon value
-        r_tot = np.float32(r_tot + r_i) + eps * cur_sign_grad #w_k_sign
+        r_i = (pert) * w / np.linalg.norm(w)
+        # combined calculated perturbation with FGSM, multiply the gradient by the epsilon value
+        r_tot = np.float32(r_tot + r_i) + eps * target_sign_grad  # w_k_sign
 
-        #If cuda is being used, store perturbed image tensor in GPU, if not, store it in CPU
+        # If cuda is being used, store perturbed image tensor in GPU, if not, store it in CPU
         if is_cuda:
             pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot).cuda()
-            pert = ((1 + overshoot) * torch.from_numpy(r_tot))*100
+            pert = ((1 + overshoot) * torch.from_numpy(r_tot)) * 100
         else:
             pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot)
             pert = ((1 + overshoot) * torch.from_numpy(r_tot))
 
-        #Add new perturbation to x
+        # Add new perturbation to x
         x = Variable(pert_image, requires_grad=True)
 
-        #Propagate x through network, get new label, get new f_k distance.
+        # Propagate x through network, get new label, get new f_k distance.
         fs = net.forward(x)
         k_i = np.argmax(fs.data.cpu().numpy().flatten())
         print('k_i:')
@@ -129,28 +149,28 @@ def targetx(image, net, eps=0.05, num_classes=1000, overshoot=0.02, max_iter=50)
         newf_k = (fs[0, k_i] - fs[0, I[0]]).data.cpu().numpy()
         loop_i += 1
 
-
     r_tot = (1 + overshoot) * r_tot
 
-    return r_tot, loop_i, label, k_i, pert_image, pert, newf_k
+    return r_tot, loop_i, orig_label, k_i, pert_image, pert, newf_k
 
-#Second version of targetx that allows a label to be chosen as function input
-def targetx_arg(image, net, inputNum, eps=0.05, num_classes=1000, overshoot=0.02, max_iter=50):
+
+# Second version of targetx that allows a label to be chosen as function input
+def targetx_arg(image, net, target_label, eps=0.05, num_classes=10, overshoot=0.02, max_iter=50):
     """
-       :param image: Image of size HxWx3
-       :param net: network (input: images, output: values of activation **BEFORE** softmax).
-       :param inputNum: Number of label to perturb image to
-       :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
-       :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
-       :param max_iter: maximum number of iterations for deepfool (default = 50)
-       :param eps: epsilon value for combination of FGSM.
-       :return: perturbation from hybrid method, number of iterations that it required, new estimated_label, perturbed image, and F_k value (distance moved into new hyperplane)
+        :param image: Image of size HxWx3
+        :param net: network (input: images, output: values of activation **BEFORE** softmax).
+        :param inputNum: Number of label to perturb image to
+        :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
+        :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
+        :param max_iter: maximum number of iterations for targetx (default = 50)
+        :param eps: epsilon value for combination of FGSM.
+        :return: perturbation from targetx method, number of iterations that it required, new estimated_label, perturbed image, and F_k value (distance moved into new hyperplane)
     """
-    #Check if cuda is available.
+    # Check if cuda is available.
     is_cuda = torch.cuda.is_available()
     ILSVRClabels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
-    #If cuda is available use GPU for faster processing, if not, use CPU.
+    # If cuda is available use GPU for faster processing, if not, use CPU.
     if is_cuda:
         print("Using GPU")
         image = image.cuda()
@@ -158,87 +178,131 @@ def targetx_arg(image, net, inputNum, eps=0.05, num_classes=1000, overshoot=0.02
     else:
         print("Using CPU")
 
-    #Convert image into tensor readable by PyTorch, flatten image.
+    # Convert image into tensor readable by PyTorch, flatten image.
     f_image = net.forward(Variable(image[None, :, :, :], requires_grad=True)).data.cpu().numpy().flatten()
-    #Create array of labels.
+
+    # Create array of labels.
     I = (np.array(f_image)).flatten().argsort()[::-1]
-    #I = np.sort(I)
-    newLabel = ILSVRClabels[np.int(inputNum)].split(',')[0]
-    print('Input ID: ' + str(inputNum), ' Label: ' + newLabel)
 
-    #Define array as size of specified number of classes, set first class to the original label.
+    print('I array: ')
+    print(I[0:num_classes])
+
+    newLabel = ILSVRClabels[np.int(target_label)].split(',')[0]
+    print('Input ID: ' + str(target_label), ' Label: ' + newLabel)
+
+    # Define array as size of specified number of classes
     I = I[0:num_classes]
-    label = I[0]
+    # Set first class to the original label.
+    orig_label = I[0]
 
-
-    #Copy the image, create variable for perturbed image, as well as w and r_tot, using the shape of the image
+    # copy image
     input_shape = image.cpu().numpy().shape
+    # create pert image variable
     pert_image = copy.deepcopy(image)
+    # create leap vector w
     w = np.zeros(input_shape)
+    # create r_tot
     r_tot = np.zeros(input_shape)
 
-    #initialize loop variable to 0
+    # initialize loop variable to 0
     loop_i = 0
 
-    #Set x to the original image, forward propagate it through the network, get list of classes
+    # create a variable x and set it to the original image
     x = Variable(pert_image[None, :], requires_grad=True)
+    # forward propogate imgae x through the network
     fs = net.forward(x)
-    fs_list = [fs[0, I[k]] for k in range(num_classes)]
-    k_i = label
+    # setting the initial k, k_i to the original lable orig_label
+    k_i = orig_label
 
-    #While label has not changed to custom label and max iterations not reached:
-    while k_i != int(inputNum) and loop_i < max_iter:
+    # While label has not changed to custom label and max iterations not reached:
+    # While label has not changed to custom label and max iterations not reached:
+    while k_i != int(target_label) and loop_i < max_iter:
 
-        #Backwards propagate label through graph, get resulting gradient and gradient sign.
+        # Backwards propagate label through graph, get resulting gradient and gradient sign.
         pert = 0  # np.inf we change the to be zero instead of infinty
         fs[0, I[0]].backward(retain_graph=True)
         grad_orig = x.grad.data.cpu().numpy().copy()
 
         zero_gradients(x)
-             #Backwards propagate current label through graph, get resulting gradient and gradient sign.
-        k = np.where(I == int(inputNum))
+
+        # Backwards propagate current label through graph, get resulting gradient and gradient sign.
+        # k is how we turn the given target label to an index value
+        k = np.where(I == int(target_label))
+        print("correct K: ")
+        print(k)
         k = k[0][0]
+        print('k')
+        print(k)
+
         fs[0, I[k]].backward(retain_graph=True)
+        grad_target = x.grad.data.cpu().numpy().copy()
+        target_sign_grad = x.grad.sign().cpu().numpy().copy()
+
+        zero_gradients(x)
+
+        fs[0, I[k]].backward(retain_graph=True)
+        print(I[k])
+        print('0, I[k]')
+        print(fs[0, I[k]])
         cur_grad = x.grad.data.cpu().numpy().copy()
         cur_sign_grad = x.grad.sign().cpu().numpy().copy()  # added for fgsm
 
-        # set new w_k and new f_k
-        w_k = cur_grad - grad_orig
+        # set new w_k, distance between original and current gradient, and new f_k, distance between original and target label
+        w = grad_target - grad_orig
         f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
 
+        # Calculate perturbation using targetx formula
+        pert = abs(f_k) / np.linalg.norm(w.flatten(), ord=1)
+        print('pert')
+        print(pert)
 
-        #Calculate perturbation using deepfool formula
-        pert_k = abs(f_k) / np.linalg.norm(w_k.flatten(), ord=1)
-
-
-        # determine which w_k to use
-        if pert_k > pert:  # we change here the "<" to be ">" to get the max hyperplanes
-            pert = pert_k
-            w = w_k
+        print('----------')
 
         # compute r_i and r_tot
-        # Added 1e-4 for numerical stability
-        r_i = (pert + 1e-4) * w / np.linalg.norm(w)
-        #combined calculated perturbation with FGSM, multiply the gradient by the epsilon value
-        r_tot = np.float32(r_tot + r_i) + eps * cur_sign_grad #w_k_sign
+        r_i = (pert) * w / np.linalg.norm(w)
+        # combined calculated perturbation with FGSM, multiply the gradient by the epsilon value
+        r_tot = np.float32(r_tot + r_i) + eps * target_sign_grad  # w_k_sign
 
-        #If cuda is being used, store perturbed image tensor in GPU, if not, store it in CPU
+        # If cuda is being used, store perturbed image tensor in GPU, if not, store it in CPU
         if is_cuda:
             pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot).cuda()
+            pert = ((1 + overshoot) * torch.from_numpy(r_tot)) * 100
         else:
             pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot)
             pert = ((1 + overshoot) * torch.from_numpy(r_tot))
 
-        #Add new perturbation to x
+        # Add new perturbation to x
         x = Variable(pert_image, requires_grad=True)
 
-        #Propagate x through network, get new label, get new f_k distance.
+        # Propagate x through network, get new label, get new f_k distance.
         fs = net.forward(x)
         k_i = np.argmax(fs.data.cpu().numpy().flatten())
+        print('k_i:')
+        print(k_i)
         newf_k = (fs[0, k_i] - fs[0, I[0]]).data.cpu().numpy()
         loop_i += 1
 
-
     r_tot = (1 + overshoot) * r_tot
 
-    return r_tot, loop_i, label, k_i, pert_image, newf_k
+    return r_tot, loop_i, orig_label, k_i, pert_image, pert, newf_k
+
+def targetx_return_I_array(image, net, num_classes=10):
+    is_cuda = torch.cuda.is_available()
+    ILSVRClabels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
+
+    # If cuda is available use GPU for faster processing, if not, use CPU.
+    if is_cuda:
+        print("Using GPU")
+        image = image.cuda()
+        net = net.cuda()
+    else:
+        print("Using CPU")
+
+    # Convert image into tensor readable by PyTorch, flatten image.
+    # f_image is the flattened image
+    f_image = net.forward(Variable(image[None, :, :, :], requires_grad=True)).data.cpu().numpy().flatten()
+    # Create array of labels I.
+    I = (np.array(f_image)).flatten().argsort()[::-1]
+    I = I[0:num_classes]
+
+    return I
