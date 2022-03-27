513e10f6d5dfc732688a146c10f352a8c74ef620
The Network & Software Security Research (NSSR) lab <86532171+nssrlab@users.noreply.github.com>
2022-03-27 18:33:48 -0400

    Update TargetXTest.py

--- TargetXTest.py
+++ TargetXTest.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision.models as models
 from PIL import Image
-from TargetX import targetx
+from TargetX import targetx, targetx_return_I_array, targetx_arg
 import os
+import time
 
 #Define test network
 net = models.resnet34(pretrained=True)
@@ -14,7 +15,7 @@ net = models.resnet34(pretrained=True)
 net.eval()
 
 #Open image
-im_orig = Image.open('new/ILSVRC2017_test_00004312.JPEG')
+im_orig = Image.open('new/ILSVRC2017_test_00004355.JPEG')
 
 mean = [ 0.485, 0.456, 0.406 ]
 std = [ 0.229, 0.224, 0.225 ]
@@ -29,14 +30,23 @@ im = transforms.Compose([
                          std = std)])(im_orig)
 
 #Run attack
+I = targetx_return_I_array(im, net, 10)
+print(I)
+start_time = time.time()
 r, loop_i, label_orig, label_pert, pert_image, pert, newf_k = targetx(im, net, 0.05)
-
+# r, loop_i, label_orig, label_pert, pert_image, pert, newf_k = targetx_arg(im, net, 318, 0.001)
+end_time = time.time()
+exec_time  = end_time-start_time
+print(exec_time)
 #Open labels
 labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
 #Get original and perturbed labels
 str_label_orig = labels[np.int(label_orig)].split(',')[0]
 str_label_pert = labels[np.int(label_pert)].split(',')[0]
+
+# str_label_orig = fs_list[np.int(label_orig)].split(',')[0]
+# str_label_pert = fs_list[np.int(label_pert)].split(',')[0]
 
 print("Original label = ", str_label_orig)
 print("Perturbed label = ", str_label_pert)
