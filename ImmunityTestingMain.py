154e1e3d75b6e1e0cec7f03328741b78b632bc02
The Network & Software Security Research (NSSR) lab <86532171+nssrlab@users.noreply.github.com>
2022-04-12 10:03:28 -0400

    Update ImmunityTestingMain.py

--- ImmunityTestingMain.py
+++ ImmunityTestingMain.py
@@ -3,13 +3,14 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch
 import torch.optim as optim
-from ImmunityTestingFunction import TargetX_Immunity_Testing
+from ImmunityTestingFunction import TargetX_Immunity_Testing, targetFGSM_Immunity_Testing
 import torchvision.models as models
 from PIL import Image
 from TargetX import targetx_arg, targetx_return_I_array
 import os
 import glob
 import random
+import art
 
 #Check if cuda is available.
 is_cuda = torch.cuda.is_available()
@@ -108,6 +109,58 @@ targetX_rnet_101_2 = models.resnet101(pretrained=True)
 targetX_rnet_101_2.cuda()
 targetX_rnet_101_2.eval()
 
+# TARGET-FGSM AlexNet
+targetfgsm_anet_0005 = models.alexnet(pretrained=True)
+targetfgsm_anet_0005.cuda()
+targetfgsm_anet_0005.eval()
+
+targetfgsm_anet_05 = models.alexnet(pretrained=True)
+targetfgsm_anet_05.cuda()
+targetfgsm_anet_05.eval()
+
+targetfgsm_anet_2 = models.alexnet(pretrained=True)
+targetfgsm_anet_2.cuda()
+targetfgsm_anet_2.eval()
+
+# TARGET-FGSM GoogleNet
+targetfgsm_gnet_0005 = models.googlenet(pretrained=True)
+targetfgsm_gnet_0005.cuda()
+targetfgsm_gnet_0005.eval()
+
+targetfgsm_gnet_05 = models.googlenet(pretrained=True)
+targetfgsm_gnet_05.cuda()
+targetfgsm_gnet_05.eval()
+
+targetfgsm_gnet_2 = models.googlenet(pretrained=True)
+targetfgsm_gnet_2.cuda()
+targetfgsm_gnet_2.eval()
+
+# TARGET-FGSM ResNet-34
+targetfgsm_rnet_34_0005 = models.resnet34(pretrained=True)
+targetfgsm_rnet_34_0005.cuda()
+targetfgsm_rnet_34_0005.eval()
+
+targetfgsm_rnet_34_05 = models.resnet34(pretrained=True)
+targetfgsm_rnet_34_05.cuda()
+targetfgsm_rnet_34_05.eval()
+
+targetfgsm_rnet_34_2 = models.resnet34(pretrained=True)
+targetfgsm_rnet_34_2.cuda()
+targetfgsm_rnet_34_2.eval()
+
+# TARGET-FGSM ResNet-101
+targetfgsm_rnet_101_0005 = models.resnet101(pretrained=True)
+targetfgsm_rnet_101_0005.cuda()
+targetfgsm_rnet_101_0005.eval()
+
+targetfgsm_rnet_101_05 = models.resnet101(pretrained=True)
+targetfgsm_rnet_101_05.cuda()
+targetfgsm_rnet_101_05.eval()
+
+targetfgsm_rnet_101_2 = models.resnet101(pretrained=True)
+targetfgsm_rnet_101_2.cuda()
+targetfgsm_rnet_101_2.eval()
+
 
 def TargetX_Immunity_Training(orig_net, immune_net, name, eps):
     if is_cuda:
@@ -177,57 +230,93 @@ def TargetX_Immunity_Training(orig_net, immune_net, name, eps):
     torch.save(immune_net.state_dict(), PATH)
     return immune_net
 
+def TargetFGSM_Immunity_Training(orig_net, immune_net, name, eps):
+    if is_cuda:
+        immune_net.cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(immune_net.parameters(), lr=1e-5)
 
-# Call functions for training and testing
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
 
-# AlexNet
-targetX_anet_0005_test = TargetX_Immunity_Training(anet, targetX_anet_0005, 'alexnet', 0.0005)
-targetX_anet_0005_test.eval()
-TargetX_Immunity_Training(anet, targetX_anet_0005_test, 0.2, 'TargetX_alexnet_Immunity_Finished_0.0005.csv')
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(orig_net.parameters(), lr=0.01)
 
-targetX_anet_05_test = TargetX_Immunity_Training(anet, targetX_anet_05, 'alexnet', 0.05)
-targetX_anet_05_test.eval()
-TargetX_Immunity_Training(anet, targetX_anet_05_test, 0.05, 'TargetX_alexnet_Immunity_Finished_0.05.csv')
+    classifier = art.estimators.classification.PyTorchClassifier(
+        model=orig_net,
+        input_shape=(3, 224, 224),
+        loss=criterion,
+        optimizer=optimizer,
+        nb_classes=1000
+    )
 
-targetX_anet_2_test = TargetX_Immunity_Training(anet, targetX_anet_2, 'alexnet', 0.2)
-targetX_anet_2_test.eval()
-TargetX_Immunity_Training(anet, targetX_anet_2_test, 0.2, 'TargetX_alexnet_Immunity_Finished_0.2.csv')
+    for epoch in range(5):
+        if epoch != 0 :
+            PATH = './targetFGSM_adv_net' + name + str(epoch) + '.pth'
+            torch.save(immune_net.state_dict(), PATH)
+            immune_net.eval()
+            csv = 'targetFGSM' + name + 'immunityepoch' + str(epoch) + str(eps) + '.csv'
+            targetFGSM_Immunity_Testing(net, immune_net, eps, csv)
+            immune_net.train()
+        running_loss = 0.0
+        i = 0
+        counter = 0
+        for filename in glob.glob('D:/Imagenet/ILSVRC2012/ILSVRC/Data/CLS-LOC/val/*.JPEG'):
+            if counter == 5000:
+                break
+            im_orig = Image.open(filename).convert('RGB')
+            im = transforms.Compose([
+                transforms.Scale(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=mean, std=std)])(im_orig)
 
-# GoogleNet
-targetX_gnet_0005_test = TargetX_Immunity_Training(gnet, targetX_gnet_0005, 'googlenet', 0.0005)
-targetX_gnet_0005_test.eval()
-TargetX_Immunity_Training(gnet, targetX_gnet_0005_test, 0.0005, 'TargetX_googlenet_Immunity_Finished_0.0005.csv')
+            I = targetx_return_I_array(im, orig_net, 10)
+            print(I)
 
-targetX_gnet_05_test = TargetX_Immunity_Training(gnet, targetX_gnet_05, 'googlenet', 0.05)
-targetX_gnet_05_test.eval()
-TargetX_Immunity_Training(gnet, targetX_gnet_05_test, 0.05, 'TargetX_googlenet_Immunity_Finished_0.05.csv')
+            # label to exclude from choice
+            exclude_label = I[0]
+            # chooses a random int in the I array
+            inputNum = random.choice(I)
+            if inputNum == exclude_label:
+                print("target label same as original label")
+                inputNum = random.choice(I)
+            targetLabel = np.array([])
+            targetLabel = np.append(targetLabel, inputNum)
 
-targetX_gnet_2_test = TargetX_Immunity_Training(gnet, targetX_gnet_2, 'googlenet', 0.2)
-targetX_gnet_2_test.eval()
-TargetX_Immunity_Training(gnet, targetX_gnet_2_test, 0.2, 'TargetX_googlenet_Immunity_Finished_0.2.csv')
+            input_batch = im.unsqueeze(0)
+            result = classifier.predict(input_batch, 1, False)
+            label_orig = np.argmax(result.flatten())
 
-# ResNet-34 testing for three epsilon values
-targetX_rnet_34_0005_test = TargetX_Immunity_Training(net, targetX_rnet_34_0005, 'resnet34', 0.0005)
-targetX_rnet_34_0005_test.eval()
-TargetX_Immunity_Testing(net, targetX_rnet_34_0005_test, 0.0005, 'TargetX_resnet34_Immunity_Finished_0.0005.csv')
+            labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
-targetX_rnet_34_05_test = TargetX_Immunity_Training(net, targetX_rnet_34_05, 'resnet34', 0.05)
-targetX_rnet_34_05_test.eval()
-TargetX_Immunity_Testing(net, targetX_rnet_34_05_test, 0.05, 'TargetX_resnet34_Immunity_Finished_0.05.csv')
+            attack = art.attacks.evasion.FastGradientMethod(estimator=classifier, eps=eps, norm=np.inf, targeted=True)
+            input_array = input_batch.numpy()
+            img_adv = attack.generate(x=input_array, y=targetLabel)
 
-targetX_rnet_34_2_test = TargetX_Immunity_Training(net, targetX_rnet_34_2, 'resnet34', 0.2)
-targetX_rnet_34_2_test.eval()
-TargetX_Immunity_Testing(net, targetX_rnet_34_2_test, 0.2, 'TargetX_resnet34_Immunity_Finished_0.2.csv')
+            result_adv = classifier.predict(img_adv, 1, False)
+            label_pert = np.argmax(result_adv.flatten())
 
-# ResNet-101 for three epsilon values
-targetX_rnet_101_0005_test = TargetX_Immunity_Training(rnet_101, targetX_rnet_101_0005, 'resnet101', 0.0005)
-targetX_rnet_101_0005_test.eval()
-TargetX_Immunity_Testing(rnet_101, targetX_rnet_101_0005_test, 0.2, 'TargetX_resnet101_Immunity_Finished_0.0005.csv')
-
-targetX_rnet_101_05_test = TargetX_Immunity_Training(rnet_101, targetX_rnet_101_05, 'resnet101', 0.05)
-targetX_rnet_101_05_test.eval()
-TargetX_Immunity_Testing(rnet_101, targetX_rnet_101_05_test, 0.2, 'TargetX_resnet101_Immunity_Finished_0.05.csv')
-
-targetX_rnet_101_2_test = TargetX_Immunity_Training(rnet_101, targetX_rnet_101_2, 'resnet101', 0.2)
-targetX_rnet_101_2_test.eval()
-TargetX_Immunity_Testing(rnet_101, targetX_rnet_101_2_test, 0.2, 'TargetX_resnet101_Immunity_Finished_0.2.csv')
+            inputs = result_adv
+            labels = ILSVRClabels[np.int(counter)].split(' ')[1]
+            labels = torch.tensor([int(labels)])
+            labels = labels.to('cuda')
+            # zero the param gradients
+            optimizer.zero_grad()
+            # forward and backward propagation and then optimization
+            outputs = immune_net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # printing the statistics
+            running_loss += loss.item()
+            # prints every 2000 mini-batches
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+            i = i + 1
+            counter = counter + 1
+    print("Finished Training")
+    PATH = 'targetFGSM_adv_net' + str(eps) + name + '.csv'
+    torch.save(immune_net.state_dict(), PATH)
+    return immune_net
